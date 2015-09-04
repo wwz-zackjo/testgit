4cbe15ec91dfc91f1ee8d9f39b5e67247f0a1644
zackjo <zackjo@naver.com>
2015-09-04 12:54:23 +0900

    메이븐 프로젝트 생

---
+++ Spring-mvn/src/main/java/com/localhose/Spring_mvn/App.java
@@ -0,0 +1,13 @@
+package com.localhose.Spring_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
